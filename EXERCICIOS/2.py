ca9a35514e65f3022a51556bad3f35aafde739c4
Pixchui <matheuzinhopintosilva@gmail.com>
2026-04-24 14:43:59 -0300

    Foi

--- EXERCICIOS/2.py
+++ EXERCICIOS/2.py
@@ -22,11 +22,10 @@ with open('EXERCICIOS/jogos.csv', 'w', encoding='utf-8') as f:
 
 df = pd.read_csv('EXERCICIOS/jogos.csv')
 
-df['genero'].value_counts()           # Minha previsão: Quantidades de um valor específico na coluna 'genero'.
+df['genero'].value_counts()            # Minha previsão: Quantidades de um valor na coluna 'genero'.
 df['nota'].max()                       # Minha previsão: Valor máximo da coluna 'nota'.
 df['preco'].min()                      # Minha previsão: Valor mínimo da coluna 'preco'.
 df.loc[5, 'jogo']                      # Minha previsão: Valor da linha 5 e coluna 'jogo'.
 df.iloc[0:3, 1:3]                      # Minha previsão: Valores das linhas 0 a 2 e colunas 1 a 2 (excluindo a coluna 3).
-df[['jogo', 'ano']].tail(3)            # Minha previsão: Valores das colunas 'jogo' e 'ano' das últimas 3 linhas.
-df.describe().loc['mean']              # Minha previsão: Média de todas as colunas numéricas do DataFrame.
-
+df[['jogo', 'ano']].tail(3)            # Minha previsão: Valores das ultimas 3 linhas das colunas 'jogo' e 'ano'.
+df.describe().loc['mean']              # Minha previsão: Média de todas as colunas numéricas.
